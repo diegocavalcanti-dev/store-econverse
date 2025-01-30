e9cfe012dea7a665a22cca2f13296b951d122997
Diego Cavalcanti <cavalcantidiego@hotmail.com>
2025-01-30 16:04:49 -0300

    feat: config Hero

--- teste-econverse/src/App.tsx
+++ teste-econverse/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./styles/global.scss"; // Importando estilos globais
+import Hero from "./components/Hero";
 
 function App () {
   return (
     <div>
       <Header />
+      <Hero />
       <main className="container">
         <h2>Vitrine de Produtos</h2>
         <div className="product-grid">
